16ecd9207628d8b1e30f1a9890fdd3b0ed1b6e3d
Amiel Martin <amiel.martin@gmail.com>
2013-11-17 18:32:15 -0800

    Home page content

---
+++ page-home.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div id="main">
+
+  <h1>Bellingham Circus Guild</h1>
+  <p id="mission">
+    We are jugglers, acrobats, aerialists, stilters, clowns, myth makers, experimental multi-medium theatricalists, and so very much more.
+  </p>
+
+  <div id="coming_soon">
+  </div>
+
+  <div id="sponsors">
+    Thank you to our sponsors:
+    <a href="">Mallard Ice Cream</a>,
+    <a href="">Casa Que Pasa</a>,
+    <a href="">Living Earth Herbs</a> ...
+  </div>
+</div>
+<?php get_footer(); ?>
