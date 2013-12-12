742fadea62179d74c39ce2d6e2d3793155975a5b
Amiel Martin <amiel.martin@gmail.com>
2013-12-11 18:03:29 -0800

    Improve the state of the footer; fix some colors

--- page-home.php
+++ page-home.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
-<div id="main">
+<div class="wrapper">
+  <div id="main">
 
-  <h1>Bellingham Circus Guild</h1>
-  <p id="mission">
-    We are jugglers, acrobats, aerialists, stilters, clowns, myth makers, experimental multi-medium theatricalists, and so very much more.
-  </p>
+    <h1>Bellingham Circus Guild</h1>
+    <p id="mission">
+      We are jugglers, acrobats, aerialists, stilters, clowns, myth makers, experimental multi-medium theatricalists, and so very much more.
+    </p>
 
-  <div id="coming_soon">
-  </div>
+    <div id="coming_soon">
+    </div>
 
-  <div id="sponsors">
-    Thank you to our sponsors:
-    <a href="">Mallard Ice Cream</a>,
-    <a href="">Casa Que Pasa</a>,
-    <a href="">Living Earth Herbs</a> ...
+    <div id="sponsors">
+      Thank you to our sponsors:
+      <a href="">Mallard Ice Cream</a>,
+      <a href="">Casa Que Pasa</a>,
+      <a href="">Living Earth Herbs</a> ...
+    </div>
   </div>
 </div>
 <?php get_footer(); ?>
