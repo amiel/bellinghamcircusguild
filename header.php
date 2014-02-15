54cf08b4a84021f89532c74ac7765b930a9572f2
Amiel Martin <amiel@carnesmedia.com>
2014-02-14 17:43:10 -0800

    Very page-home. Much style. WOW.

--- header.php
+++ header.php
@@ -21,5 +21,5 @@
   </div>
 
   <h1 id="logo">
-    <a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
+    <a href="//<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
   </h1>
--- page-home.php
+++ page-home.php
@@ -2,20 +2,35 @@
 <div class="wrapper">
   <div id="main">
 
-    <h1>Bellingham Circus Guild</h1>
-    <p id="mission">
-      We are jugglers, acrobats, aerialists, stilters, clowns, myth makers, experimental multi-medium theatricalists, and so very much more.
-    </p>
+    <section id="mission">
+      <h2>Bellingham Circus Guild</h2>
+      <p>We are jugglers, acrobats, aerialists, stilters, clowns, myth makers, experimental multi-medium theatricalists, and so very much more.</p>
+    </section>
 
-    <div id="coming_soon">
-    </div>
+    <section id="coming-soon">
+      <h3>Coming Soon</h3>
+      <h4>to the Cirque Lab</h4>
 
-    <div id="sponsors">
+      <hr />
+
+      <a href="#">
+        <!-- stub content -->
+        <!-- TODO: Use ACF -->
+
+        <time>August 24</time>
+        / <span class="description">Super Hero Boy Band with special guests Funky Monkey and the New Loompahs</span>
+        <img src="<?= bloginfo('stylesheet_directory') ?>/images/arrow-yellow.png" />
+      </a>
+    </section>
+
+    <section id="sponsors">
       Thank you to our sponsors:
-      <a href="">Mallard Ice Cream</a>,
-      <a href="">Casa Que Pasa</a>,
-      <a href="">Living Earth Herbs</a> ...
-    </div>
+      <span class="links">
+        <a href="#">Mallard Ice Cream</a>,
+        <a href="#">Casa Que Pasa</a>,
+        <a href="#">Living Earth Herbs</a> ...
+      </span>
+    </section>
   </div>
 </div>
 <?php get_footer(); ?>
