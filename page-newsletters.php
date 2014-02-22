98e45feb606880e091e5933e13394de369b5bc85
Amiel Martin <amiel@carnesmedia.com>
2014-02-22 11:17:11 -0800

    Newsletters page lists mailchimp campaigns

---
+++ page-newsletters.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div class="wrapper">
+  <div id="main">
+
+    <style type="text/css">
+    <!--
+    .display_archive {font-family: arial,verdana; font-size: 12px;}
+    .campaign {line-height: 125%; margin: 5px;}
+    //-->
+    </style>
+    <script language="javascript" src="http://us1.campaign-archive1.com/generate-js/?u=88ad4bffeee977ac0ee07d20e&fid=48149&show=10" type="text/javascript"></script>
+
+  </div>
+</div>
+<?php get_footer(); ?>
