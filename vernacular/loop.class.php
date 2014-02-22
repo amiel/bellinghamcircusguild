241dc7b0583430935c7fa9e3af4c0e5435d5185d
Amiel Martin <amiel@carnesmedia.com>
2014-02-22 13:42:07 -0800

    Add support for custom post type queries

--- vernacular/loop.class.php
+++ vernacular/loop.class.php
@@ -1,54 +1,67 @@
 <?php
 class VernacularQuery{
-  public $tags;
-  public $post_type = 'post';
-  public $categories;
+  public $default_post_type = 'post';
 
-  public function random_posts($count = 3){
-    $options = array('posts_per_page' => $count, 'orderby' => 'rand');
-    return $this->query($options);
+  public function __construct($options) {
+    $this->options = $options;
+    if (!$this->options['post_type'])
+      $this->options['post_type'] = $this->default_post_type;
   }
 
-  public function recent_posts($count = 3){
-    $options = array('posts_per_page' => $count);
-    return $this->query($options);
-  }
+  // public function random_posts($count = 3){
+  //   $options = array('posts_per_page' => $count, 'orderby' => 'rand');
+  //   return $this->query($options);
+  // }
 
-  private function query($options){
-    $options = array_merge(array(
-      'post_type' => $this->post_type,
-      'tag' => $this->tags,
-      'category_name' => $this->categories
-    ), $options);
+  // public function recent_posts($count = 3){
+  //   $this->add_option('posts_per_page', $count);
+  //   return $this->query();
+  // }
 
-    $query = new WP_Query($options);
+  public function query(){
+    $query = new WP_Query($this->options);
     return $query->get_posts();
   }
 
   // Options
   public function post_type($post_type){
-    $this->post_type = $post_type;
-    return $this;
+    return $this->add_option('post_type', $post_type);
   }
 
-  public function tags($tags){
-    $this->tags = $tags;
-    return $this;
+  public function tags($tags) {
+    return $this->add_option('tag', $tags);
   }
 
   public function categories($categories){
-    $this->categories = $categories;
-    return $this;
+    return $this->add_option('category_name', $categories);
+  }
+
+  public function limit($count) {
+    return $this->add_option('posts_per_page', $count);
+  }
+
+  public function add_option($key, $value) {
+    return new VernacularQuery(array_merge($this->options, array($key => $value)));
+  }
+
+  public function order($by, $direction) {
+    return $this->add_option('orderby', $by)->add_option('order', $direction);
+  }
+
+  public function custom_type_query($key, $compare, $value) {
+    return $this->add_option('meta_key', $key)
+      ->add_option('meta_value', $value)
+      ->add_option('meta_compare', $compare);
   }
 }
 class_alias('VernacularQuery', '_query');
 
 function VernacularQuery(){
-  return new VernacularQuery;
+  return new VernacularQuery(array());
 }
 
 function _query(){
-  return new VernacularQuery;
+  return new VernacularQuery(array());
 }
 
 class VernacularLoop{
