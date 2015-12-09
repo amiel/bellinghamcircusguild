d770a3c8196c85edef2fe0ddd0805d4e1da6dedb
Amiel Martin <amiel@actionsprout.com>
2015-12-08 21:07:36 -0800

    Use sendgrid (with curl) to send email
    BCG_SENDGRID_API_KEY must be defined in wp-config.php

--- modules/email.php
+++ modules/email.php
@@ -1,15 +1,10 @@
 <?php
 
+require('../../../wp-config.php');
+
 function send_email($subject, $email_message, $reply_to, $honeypot = "") {
   if (empty($honeypot)) {
-    $to = "belingham-circus-guild@googlegroups.com, website@bellinghamcircusguild.com";
-    /* $to = "amiel.martin@gmail.com"; */
-
-    $headers = 'From: "Guild Website" <website@bellinghamcircusguild.com>' . "\r\n" .
-        "Reply-To: $reply_to" . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $email_message, $headers);
+    _do_send_email($subject, $email_message, $reply_to);
   } else {
     // You fell in to the honey pot!
     // Nope, no email sending for you!
@@ -17,4 +12,31 @@ function send_email($subject, $email_message, $reply_to, $honeypot = "") {
 
   $url = "/contact/thanks/";
   header("Location: $url\r\n");
+}
+
+function _do_send_email($subject, $message, $reply_to) {
+  /* $to = "belingham-circus-guild@googlegroups.com"; */
+  /* $cc = "website@bellinghamcircusguild.com"; */
+  $to = "amiel.martin@gmail.com";
+
+  $api_key = constant('BCG_SENDGRID_API_KEY');
+  $url = 'https://api.sendgrid.com/api/mail.send.json';
+
+  $params = array(
+    'to' => $to, 'cc' => $cc, 'subject' => $subject, 'replyto' => $reply_to,
+    'fromname' => "BCG Website", 'from' => 'website@bellinghamcircusguild.com',
+    'text' => $message,
+  );
+
+  $headers = array("Authorization: Bearer $api_key");
+
+  $session = curl_init($url);
+  curl_setopt($session, CURLOPT_POST, true);
+  curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+  curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($session, CURLOPT_POSTFIELDS, $params);
+  curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
+
+  $response = curl_exec($session);
+  curl_close($session);
 }
