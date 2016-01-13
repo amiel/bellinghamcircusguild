03e427535e97bf32b73e30a1dbb4619372b0091f
Amiel Martin <amiel@actionsprout.com>
2016-01-13 08:09:50 -0800

    Remove donate call to action on the events page

--- page-events.php
+++ page-events.php
@@ -47,14 +47,6 @@
       <section id="no-events" class="alert">
         <h3>Uh Oh!</h3>
         <h4>There are no other upcoming events to display.</h4>
-
-        <hr />
-
-        <a href="/support-us/donate">
-          <span class="description">You should donate so we can make even more fun events for you to attend!</span>
-          <span class="highlight">DONATE</span>
-          <img src="<?= bloginfo('stylesheet_directory') ?>/images/arrow-yellow.png" class="arrow" />
-        </a>
       </section>
     <?php endif; ?>
   </section>
