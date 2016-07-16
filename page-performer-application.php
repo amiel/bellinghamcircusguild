e3645961bf34bd3c4c82b47b052714b064668fc6
Amiel Martin <amiel@actionsprout.com>
2016-07-15 21:15:14 -0800

    Update Vaudevillingham form

--- page-performer-application.php
+++ page-performer-application.php
@@ -8,7 +8,7 @@
         <?php the_content(); ?>
       </div>
 
-      <iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dGpjSzM1ak45VXZYNkVuLXRscVc0aHc6MQ" width="600" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading&#8230;</iframe>
+      <iframe src="https://docs.google.com/forms/d/1L4bsvT04cbZCXOf1990CP0SGWmbxi4Q7NbQOqcY-fuw/viewform?embedded=true" width="600" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 
     </section>
     <?php get_sidebar(); ?>
